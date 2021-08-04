f80a19a152345f1a86c797a646387a15bc8912f3
externit <igorkar777@gmail.com>
2021-08-04 12:23:15 +0300

    Updates

---
+++ precache-manifest.4fc0c96e385961c62a6cf40022558a85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1044f9ba16132b56539f0a7ad57310b",
+    "url": "/crwn-clothe/index.html"
+  },
+  {
+    "revision": "2e425f6b838dc1c7e76e",
+    "url": "/crwn-clothe/static/css/main.f95d6b8c.chunk.css"
+  },
+  {
+    "revision": "d4e660fb7e430b9a98be",
+    "url": "/crwn-clothe/static/js/2.a0d28c5a.chunk.js"
+  },
+  {
+    "revision": "2e425f6b838dc1c7e76e",
+    "url": "/crwn-clothe/static/js/main.5159820a.chunk.js"
+  },
+  {
+    "revision": "b5511961fda4790109db",
+    "url": "/crwn-clothe/static/js/runtime~main.b6860bc4.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothe/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothe/static/media/shopping-bag.30f07071.svg"
+  }
+]);
